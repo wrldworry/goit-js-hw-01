313302db5419337a39122ee87be6bb22aa249301
wrldworry <wrldworry@gmail.com>
2025-04-29 20:57:07 +0300

    commit

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingValue = parseFloat(padding);
+    const borderValue = parseFloat(border);
+  
+    return contentWidth + paddingValue * 2 + borderValue * 2;
+  }
+  
+  // Перевірка роботи функції
+  console.log(getElementWidth("50px", "8px", "4px")); // 74
+  console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+  console.log(getElementWidth("200px", "0px", "0px")); // 200
